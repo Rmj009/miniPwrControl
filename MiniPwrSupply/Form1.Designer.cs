b0236f7e169b65e3cbf544dff18c4bfc6ec82f21
= <=>
2022-09-15 13:37:29 +0800

    retry

---
+++ MiniPwrSupply/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace MiniPwrSupply
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.grpBox_port_setting = new System.Windows.Forms.GroupBox();
+            this.label_OVP = new System.Windows.Forms.Label();
+            this.label_com = new System.Windows.Forms.Label();
+            this.cmbx_baudrate = new System.Windows.Forms.ComboBox();
+            this.cmbx_com = new System.Windows.Forms.ComboBox();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.grpBox_dashboard = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_sendcmd = new System.Windows.Forms.Button();
+            this.txtbx_ovp = new System.Windows.Forms.TextBox();
+            this.txtbx_Vset = new System.Windows.Forms.TextBox();
+            this.label_Vset = new System.Windows.Forms.Label();
+            this.label_Iset = new System.Windows.Forms.Label();
+            this.label_OCP = new System.Windows.Forms.Label();
+            this.txtbx_ocp = new System.Windows.Forms.TextBox();
+            this.txtbx_Iset = new System.Windows.Forms.TextBox();
+            this.label_Vin = new System.Windows.Forms.Label();
+            this.txtbx_Vin = new System.Windows.Forms.TextBox();
+            this.grpBox_port_setting.SuspendLayout();
+            this.grpBox_dashboard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBox_port_setting
+            // 
+            this.grpBox_port_setting.AutoSize = true;
+            this.grpBox_port_setting.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.grpBox_port_setting.Controls.Add(this.btn_sendcmd);
+            this.grpBox_port_setting.Controls.Add(this.label_com);
+            this.grpBox_port_setting.Controls.Add(this.cmbx_baudrate);
+            this.grpBox_port_setting.Controls.Add(this.cmbx_com);
+            this.grpBox_port_setting.Controls.Add(this.btn_open);
+            this.grpBox_port_setting.Controls.Add(this.btn_refresh);
+            this.grpBox_port_setting.Dock = System.Windows.Forms.DockStyle.Left;
+            this.grpBox_port_setting.Location = new System.Drawing.Point(0, 0);
+            this.grpBox_port_setting.Name = "grpBox_port_setting";
+            this.grpBox_port_setting.Size = new System.Drawing.Size(222, 510);
+            this.grpBox_port_setting.TabIndex = 0;
+            this.grpBox_port_setting.TabStop = false;
+            this.grpBox_port_setting.Text = "PortSetting";
+            // 
+            // label_OVP
+            // 
+            this.label_OVP.AutoSize = true;
+            this.label_OVP.Location = new System.Drawing.Point(38, 58);
+            this.label_OVP.Name = "label_OVP";
+            this.label_OVP.Size = new System.Drawing.Size(35, 15);
+            this.label_OVP.TabIndex = 4;
+            this.label_OVP.Text = "OVP";
+            // 
+            // label_com
+            // 
+            this.label_com.AutoSize = true;
+            this.label_com.Location = new System.Drawing.Point(15, 48);
+            this.label_com.Name = "label_com";
+            this.label_com.Size = new System.Drawing.Size(31, 15);
+            this.label_com.TabIndex = 4;
+            this.label_com.Text = "com";
+            // 
+            // cmbx_baudrate
+            // 
+            this.cmbx_baudrate.FormattingEnabled = true;
+            this.cmbx_baudrate.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.cmbx_baudrate.Location = new System.Drawing.Point(95, 112);
+            this.cmbx_baudrate.Name = "cmbx_baudrate";
+            this.cmbx_baudrate.Size = new System.Drawing.Size(121, 23);
+            this.cmbx_baudrate.TabIndex = 3;
+            // 
+            // cmbx_com
+            // 
+            this.cmbx_com.FormattingEnabled = true;
+            this.cmbx_com.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6"});
+            this.cmbx_com.Location = new System.Drawing.Point(95, 45);
+            this.cmbx_com.Name = "cmbx_com";
+            this.cmbx_com.Size = new System.Drawing.Size(121, 23);
+            this.cmbx_com.TabIndex = 3;
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(129, 187);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(87, 53);
+            this.btn_open.TabIndex = 2;
+            this.btn_open.Text = "open";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Location = new System.Drawing.Point(6, 187);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(87, 53);
+            this.btn_refresh.TabIndex = 2;
+            this.btn_refresh.Text = "refresh";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // grpBox_dashboard
+            // 
+            this.grpBox_dashboard.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.grpBox_dashboard.Controls.Add(this.txtbx_Iset);
+            this.grpBox_dashboard.Controls.Add(this.txtbx_Vset);
+            this.grpBox_dashboard.Controls.Add(this.richTextBox1);
+            this.grpBox_dashboard.Controls.Add(this.txtbx_Vin);
+            this.grpBox_dashboard.Controls.Add(this.label_Vin);
+            this.grpBox_dashboard.Controls.Add(this.txtbx_ocp);
+            this.grpBox_dashboard.Controls.Add(this.label_OCP);
+            this.grpBox_dashboard.Controls.Add(this.txtbx_ovp);
+            this.grpBox_dashboard.Controls.Add(this.label_Iset);
+            this.grpBox_dashboard.Controls.Add(this.label_OVP);
+            this.grpBox_dashboard.Controls.Add(this.label_Vset);
+            this.grpBox_dashboard.Location = new System.Drawing.Point(241, 12);
+            this.grpBox_dashboard.Name = "grpBox_dashboard";
+            this.grpBox_dashboard.Size = new System.Drawing.Size(686, 477);
+            this.grpBox_dashboard.TabIndex = 1;
+            this.grpBox_dashboard.TabStop = false;
+            this.grpBox_dashboard.Text = "Dashboard";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(41, 175);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(611, 272);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_sendcmd
+            // 
+            this.btn_sendcmd.Location = new System.Drawing.Point(33, 385);
+            this.btn_sendcmd.Name = "btn_sendcmd";
+            this.btn_sendcmd.Size = new System.Drawing.Size(141, 104);
+            this.btn_sendcmd.TabIndex = 2;
+            this.btn_sendcmd.Text = "_sendCmd";
+            this.btn_sendcmd.UseVisualStyleBackColor = true;
+            this.btn_sendcmd.Click += new System.EventHandler(this.btn_sendcmd_Click);
+            // 
+            // txtbx_ovp
+            // 
+            this.txtbx_ovp.Location = new System.Drawing.Point(135, 55);
+            this.txtbx_ovp.Name = "txtbx_ovp";
+            this.txtbx_ovp.Size = new System.Drawing.Size(95, 25);
+            this.txtbx_ovp.TabIndex = 0;
+            // 
+            // txtbx_Vset
+            // 
+            this.txtbx_Vset.Location = new System.Drawing.Point(135, 101);
+            this.txtbx_Vset.Name = "txtbx_Vset";
+            this.txtbx_Vset.Size = new System.Drawing.Size(95, 25);
+            this.txtbx_Vset.TabIndex = 0;
+            // 
+            // label_Vset
+            // 
+            this.label_Vset.AutoSize = true;
+            this.label_Vset.Location = new System.Drawing.Point(38, 111);
+            this.label_Vset.Name = "label_Vset";
+            this.label_Vset.Size = new System.Drawing.Size(32, 15);
+            this.label_Vset.TabIndex = 4;
+            this.label_Vset.Text = "Vset";
+            // 
+            // label_Iset
+            // 
+            this.label_Iset.AutoSize = true;
+            this.label_Iset.Location = new System.Drawing.Point(247, 111);
+            this.label_Iset.Name = "label_Iset";
+            this.label_Iset.Size = new System.Drawing.Size(27, 15);
+            this.label_Iset.TabIndex = 4;
+            this.label_Iset.Text = "Iset";
+            // 
+            // label_OCP
+            // 
+            this.label_OCP.AutoSize = true;
+            this.label_OCP.Location = new System.Drawing.Point(247, 58);
+            this.label_OCP.Name = "label_OCP";
+            this.label_OCP.Size = new System.Drawing.Size(34, 15);
+            this.label_OCP.TabIndex = 4;
+            this.label_OCP.Text = "OCP";
+            // 
+            // txtbx_ocp
+            // 
+            this.txtbx_ocp.Location = new System.Drawing.Point(344, 55);
+            this.txtbx_ocp.Name = "txtbx_ocp";
+            this.txtbx_ocp.Size = new System.Drawing.Size(95, 25);
+            this.txtbx_ocp.TabIndex = 0;
+            // 
+            // txtbx_Iset
+            // 
+            this.txtbx_Iset.Location = new System.Drawing.Point(344, 101);
+            this.txtbx_Iset.Name = "txtbx_Iset";
+            this.txtbx_Iset.Size = new System.Drawing.Size(95, 25);
+            this.txtbx_Iset.TabIndex = 0;
+            // 
+            // label_Vin
+            // 
+            this.label_Vin.AutoSize = true;
+            this.label_Vin.Location = new System.Drawing.Point(478, 58);
+            this.label_Vin.Name = "label_Vin";
+            this.label_Vin.Size = new System.Drawing.Size(28, 15);
+            this.label_Vin.TabIndex = 4;
+            this.label_Vin.Text = "Vin";
+            // 
+            // txtbx_Vin
+            // 
+            this.txtbx_Vin.Location = new System.Drawing.Point(575, 55);
+            this.txtbx_Vin.Name = "txtbx_Vin";
+            this.txtbx_Vin.Size = new System.Drawing.Size(95, 25);
+            this.txtbx_Vin.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(986, 510);
+            this.Controls.Add(this.grpBox_dashboard);
+            this.Controls.Add(this.grpBox_port_setting);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpBox_port_setting.ResumeLayout(false);
+            this.grpBox_port_setting.PerformLayout();
+            this.grpBox_dashboard.ResumeLayout(false);
+            this.grpBox_dashboard.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox grpBox_port_setting;
+        private System.Windows.Forms.GroupBox grpBox_dashboard;
+        private System.Windows.Forms.Button btn_refresh;
+        private System.Windows.Forms.Label label_OVP;
+        private System.Windows.Forms.Label label_com;
+        private System.Windows.Forms.ComboBox cmbx_baudrate;
+        private System.Windows.Forms.ComboBox cmbx_com;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_sendcmd;
+        private System.Windows.Forms.TextBox txtbx_Vset;
+        private System.Windows.Forms.TextBox txtbx_ovp;
+        private System.Windows.Forms.Label label_Vset;
+        private System.Windows.Forms.TextBox txtbx_Iset;
+        private System.Windows.Forms.TextBox txtbx_Vin;
+        private System.Windows.Forms.Label label_Vin;
+        private System.Windows.Forms.TextBox txtbx_ocp;
+        private System.Windows.Forms.Label label_OCP;
+        private System.Windows.Forms.Label label_Iset;
+    }
+}
+
---
+++ MiniPwrSupply/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+
+namespace MiniPwrSupply
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static Mutex mutex = new Mutex();
+        private volatile bool mGet_Start;
+        private StringBuilder sb = new StringBuilder();
+        private bool mIsConnectedSerialPort = false;
+
+        public bool ReadySet
+        {
+            get
+            {
+                bool isStart = false;
+                mutex.WaitOne();
+                isStart = mGet_Start;
+                mutex.ReleaseMutex();
+                return isStart;
+            }
+            set
+            {
+                mutex.WaitOne();
+                mGet_Start = value;
+                mutex.ReleaseMutex();
+            }
+        }
+
+        private void ShowErrMsg(string errMsg)
+        {
+            MessageBox.Show(errMsg, @"Error Occur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btn_open.Enabled = false;
+                richTextBox1.Clear();
+                if (!mIsConnectedSerialPort)
+                {
+                    this.serialPort1 = new System.IO.Ports.SerialPort(@"COM" + this.cmbx_com.Text, Convert.ToInt32(this.cmbx_baudrate), Parity.None, 8, StopBits.One);
+                    serialPort1.ReadTimeout = 100;
+                    serialPort1.DataReceived += serialport1_DataReceived;
+                    this.mIsConnectedSerialPort = true;
+                }
+                richTextBox1.AppendText("SerialPort --->" + this.serialPort1.PortName + "\nBaudRate:" + this.serialPort1.BaudRate);
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    serialPort1.Dispose();
+                    System.Threading.Thread.Sleep(200);
+                }
+                serialPort1.Open();
+                richTextBox1.AppendText("\nSerialPort is open");
+                System.Threading.Thread.Sleep(500);
+                serialPort1.Write(@"#START%");
+                System.Threading.Thread.Sleep(300);
+                int length = serialPort1.BytesToRead;
+                string receiveData = string.Empty;
+                if (true)
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowErrMsg("connect serialport ERR");
+                throw ex;
+            }
+        }
+
+        private void serialport1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int length = serialPort1.BytesToRead;
+            string recevicedata = string.Empty;
+            string datatime = string.Empty;
+            try
+            {
+                //if (IsFirstTest == true)
+                //{
+                //    receiveCall.Clear();
+                //    receiveCall.Append(receivedata);
+
+                //    Thread.Sleep(1000);
+                //    serialPort1.Write(@"#START%");
+                //    richTextBox1.AppendText(@"Receive #START%");
+                //    Thread.Sleep(200);
+
+                //    receiveCall.Clear();
+                //    IsFirstTest = false;
+                //}
+                if (length != 0)
+                {
+                    byte[] buff = new byte[length];
+                    serialPort1.Read(buff, 0, length);
+                    recevicedata = Encoding.Default.GetString(buff);
+                }
+                switch (recevicedata)
+                {
+                    //case String a when a.Contains()
+
+                    //default:
+                    //    break;
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowErrMsg(@"serialport1_DataReceived Err" + ex.Message);
+                throw ex;
+            }
+        }
+
+        private void _SendCmd(byte[] cmd, ref byte[] result) //, Binary_Serail_DLL.BinaryFormatCheck callback = null
+        {
+            Console.WriteLine("[ SEND ] " + this._ByteArrayToString(cmd).Trim());
+            //this.Save_LOG_data("[ SEND ] " + this._ByteArrayToString(cmd).Trim());
+            result = null;
+            //Binary_Serail_DLL.BINARY_SERIAL_RESULT serialResult = new Binary_Serail_DLL.BINARY_SERIAL_RESULT();
+            IntPtr callbackPtr = IntPtr.Zero;
+            //if (callback == null)
+            //{
+            //    callbackPtr = Marshal.GetFunctionPointerForDelegate(this.mBinaryCheckCallback);
+            //    callbackPtr = Marshal.GetFunctionPointerForDelegate(this.mBinaryCheckCallback);
+            //}
+            //else
+            //{
+            //    callbackPtr = Marshal.GetFunctionPointerForDelegate(callback);
+            //}
+
+            //if (!Binary_Serail_DLL.Binary_TransferReceive(cmd, (uint)cmd.Length, callbackPtr, ref serialResult, SERIAL_TIME_OUT))
+            //{
+            //    throw new Exception("Serial Error !! Command : " + this._ByteArrayToString(cmd).Trim());
+            //}
+
+            //result = new byte[serialResult.readLen];
+            //Buffer.BlockCopy(serialResult.readData, 0, result, 0, serialResult.readLen);
+
+            Console.WriteLine("[--RESP--] " + this._ByteArrayToString(result).Trim());
+            //this.Save_LOG_data("[  RESP  ] " + this._ByteArrayToString(result).Trim());
+        }
+
+        private string _ByteArrayToString(byte[] bytes)
+        {
+            StringBuilder hex = new StringBuilder(bytes.Length * 2);
+            foreach (byte b in bytes)
+            {
+                string hexStr = String.Format("{0:x2} ", b);
+                //hex.AppendFormat("{0:x2} ", b);
+                hex.Append(hexStr.ToUpper());
+            }
+
+            return hex.ToString();
+        }
+
+        private void btn_sendcmd_Click(object sender, EventArgs e)
+        {
+            string sResult = string.Empty;
+            string cmdName = "Voltage_current";
+            try
+            {
+                byte[] cmd = new byte[20];
+                cmd[0] = 0xAA;
+                cmd[1] = 0x01;
+                cmd[2] = 0x2C;
+                cmd[3] = 0x14;
+                cmd[4] = 0x50;
+                cmd[5] = 0x14;
+                cmd[6] = 0x50;
+                cmd[7] = 0x00;
+                cmd[8] = 0xC8;
+                cmd[9] = 0x0C;
+                cmd[10] = 0x80;
+                cmd[11] = 0x00;
+                cmd[12] = 0x00;
+                cmd[13] = 0x00;
+                cmd[14] = 0x42;
+                cmd[15] = 0x00;
+                cmd[16] = 0x00;
+                cmd[17] = 0x00;
+                cmd[18] = 0x00;
+                cmd[19] = 0x35;
+                byte[] result = null;
+                this._SendCmd(cmd, ref result);
+                sResult = this._ByteArrayToString(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("cmd Err!!" + ex.Message);
+            }
+        }
+    }
+}
