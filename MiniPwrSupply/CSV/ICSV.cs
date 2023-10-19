75722a965170a5fec9a41ae277dee2fc82bb13af
koukira <quanta10811053@gmail.com>
2023-10-20 00:40:56 +0800

    add ICsv.cs

---
+++ MiniPwrSupply/CSV/ICSV.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MiniPwrSupply.CSV
+{
+    internal interface ICsv
+    {
+        void Clear();
+
+        void SetCsvTempFilePath(string tempPath);
+
+        void SetCsvReleaseFilePath(string releasePath);
+
+        void SetIgnoreResultTitle(string item);
+
+        void AddTestResultItem(Object item);
+
+        void ChangeLastResultErrorCodeAndException(string errorCode, string err);
+
+        void ChangeLastResultTrayData(string trayDutSN, string trayName, string trayX, string trayY);
+
+        void SetBeginTestTime(string value);
+
+        void SetEndTestTime(string value);
+
+        void CalcFinalResult();
+
+        void GetCalcFinishResult(ref double fYieldRate, ref int total, ref int pass, ref int fail);
+
+        string GetLastResult();
+
+        string GetResultTitleJSON();
+
+        string GetResultLimitUpperJson();
+
+        string GetResultLimitLowerJson();
+
+        void DebugShowAll();
+
+        void DeleteEmptyTestResultCsv();
+    }
+}
