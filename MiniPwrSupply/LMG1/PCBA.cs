370745ff5ea71605ab51b52a17bb488f5eb88986
koukira <quanta10811053@gmail.com>
2023-12-11 00:37:35 +0800

    LMG1 add ChkEMMC()

--- MiniPwrSupply/LMG1/PCBA.cs
+++ MiniPwrSupply/LMG1/PCBA.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,18 @@ namespace MiniPwrSupply.LMG1
 {
     internal class PCBA
     {
+        public void ChkEMMC()
+        {
+            string res = "";
+            try 
+            {
+                SendAndChk(PortType.SSH, "mmc info", keyword, out res, 0, 5000);
+                SendAndChk(PortType.SSH, "reset", keyword, out res, 0, 5000);
+            }
+            catch (Exception e)
+            {
+                Display()
+            }
+        }
     }
 }
